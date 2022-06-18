9677b5512387a31ed3114b3ce02511e18842c7ed
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-06-18 13:22:42 -0400

    server updated
    updates include:
    jwt
    cookies
    req & reply
    adding routes

--- server/src/utils/createServer.ts
+++ server/src/utils/createServer.ts
@@ -1,11 +1,64 @@
-import fastify from "fastify"
+import cors from "@fastify/cors";
+import fastify from "fastify";
+import jwt from "@fastify/jwt";
+import cookie from "@fastify/cookie";
+import fs from "fs";
+import path from "path";
+import { CORS_ORIGIN } from "../constants";
+import { FastifyRequest } from "fastify";
+import { FastifyReply } from "fastify";
+import userRoutes from "../modules/user/user.route";
+import vaultRoutes from "../modules/vault/vault.route";
 
 function createServer() {
+  const app = fastify();
 
-    const app = fastify()
-    
-    return app
+  // plugins
+  app.register(cors, {
+    origin: CORS_ORIGIN,
+    credentials: true,
+  });
 
+  app.register(jwt, {
+    secret: {
+      private: fs.readFileSync(
+        `${(path.join(process.cwd()), "certs")}/private.key`
+      ),
+      public: fs.readFileSync(
+        `${(path.join(process.cwd()), "certs")}/public.key`
+      ),
+    },
+    sign: { algorithm: "RS256" },
+    cookie: {
+      cookieName: "token",
+      signed: false,
+    },
+  });
+
+  app.register(cookie, {
+    parseOptions: {},
+  });
+
+  // auth decorators
+  app.decorate(
+    "authenticate",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      try {
+        const user = await request.jwtVerify<{
+          _id: string;
+        }>();
+
+        request.user = user;
+      } catch (e) {
+        return reply.send(e);
+      }
+    }
+  );
+
+  app.register(userRoutes, { prefix: "api/users" });
+  app.register(vaultRoutes, { prefix: "api/vault" });
+
+  return app;
 }
 
-export default createServer
+export default createServer;
